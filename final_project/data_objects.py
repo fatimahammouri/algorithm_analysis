eaa398bb58f40ed6e0940e036a62042ccaa9ea55
Fatima <fatima.hammouri@gmail.com>
2023-09-22 12:13:14 -0700

    algorithm theory analysis and code

---
+++ final_project/data_objects.py
@@ -0,0 +1,3 @@
+NAMES = ['Beth Stephens Sr']
+DATES = ['1968-12-18']
+SSN = ['766-73-9985']
---
+++ final_project/tree_traversal_algorithm.py
@@ -0,0 +1,29 @@
+from final_data_objects import DATES, NAMES, SSN, ROOT
+
+def merge_sort(array):
+  if len(array) <= 1:
+    return array
+  mid = len(array) // 2
+  right_array = array[mid:]
+  left_array = array[:mid]
+  left = merge_sort(left_array)
+  right = merge_sort(right_array)
+  return merge(left, right)
+
+def merge(left, right):
+  new_array = []
+  i, j = 0, 0
+  while i < len(left) and j < len(right):
+    if left[i] <= right[j]:
+      new_array.append(left[i])
+      i += 1
+    else:
+      new_array.append(right[j])
+      j += 1
+  new_array.extend(left[i:])
+  new_array.extend(right[j:])
+  return new_array 
+
+print(merge_sort(Date))
+
+    
